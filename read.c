23ebae93a0b9b0d061feb7b7314769a9f4371184
22332371 <105031287+22332371@users.noreply.github.com>
2022-09-07 14:17:54 +0800

    Added some error handling but need to suss how to implement it with tokens etc

--- read.c
+++ read.c
@@ -7,42 +7,41 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LINES 100
+#define MAX_LENGTH 40
+
 /*
- crontab-file
- minutes - int (0 to 59)
- hours - int (0 to 24)
- days - int (0 to 31) this is dependent on the day obviously
- month - int (0 to 12)
- day_name - string(Sun, Mon, Tues etc)
- command - string
+crontab-file
+minutes - int (0 to 59)
+hours - int (0 to 24)
+days - int (0 to 31) this is dependent on the day obviously
+month - int (0 to 12)
+day_name - string(Sun, Mon, Tues etc)
+command - string
  
- estimates- file
- minutes - int
- command - string
+estimates- file
+minutes - int
+command - string
  
 
- * Open and read crontab-file and estimates-file
- 
- * Each contain at most 20 Non-comment lines and each line will be at most 100 characters long and each command name will be atleast 40 charaters long
 
- * Check for data errors in both files - if any errors only the first should be reported and then the program should terminate
  
- * Every command name appearing in the crontab-file will also appear in the estimates-file
+* Every command name appearing in the crontab-file will also appear in the estimates-file
  
- * Skip all "comment" lines that begin with a - #
+* Skip all "comment" lines that begin with a - #
  
- * The project should simuate the execution of the commands in the crontab-file for the month specified onn the command-line. (No consideration for leap-year or leap seconds needed)
+* The project should simuate the execution of the commands in the crontab-file for the month specified onn the command-line. (No consideration for leap-year or leap seconds needed)
  
- * At most 20 processes will ever be executing at the same time (during the same minute) and mutilple instances of the same command may run at the same time
+* At most 20 processes will ever be executing at the same time (during the same minute) and mutilple instances of the same command may run at the same time
  
- * The program only needs to simulate the execution pf processes for the requested month and current year and all processes terminated before the end of the requested month
+* The program only needs to simulate the execution pf processes for the requested month and current year and all processes terminated before the end of the requested month
 
- * Given the month we need to determine - commands that run in that month nd count how many times each command runs
+* Given the month we need to determine - commands that run in that month nd count how many times each command runs
  
 */
 
 
- struct Program{
+struct Program{
      int minutes;
      char[41] name;
  }
@@ -51,40 +50,56 @@
  //    char[41] name;
  //};
 
-
- struct Program programs[20];
+struct Program programs[20];
  //struct Program programs[20];
 
- void readfile(char file[]){
 
-     char tokens[40][100];
-     int i = 0;
-     
-     //OPENING THE FILE READ ONLY LINE BY LINE
-     FILE *the_file = fopen(file.txt,"r");
-     //PRINT ERROR IF UNABLE TO OPEN FILE
-     if(the_file == NULL){
-         perror("Unable to open the file");
+//* OPEN AND READ THE crontab-file AND estimates-file
+void readfile(char *file_name[]){
+
+    //STORE THE LINES FROM THE FILE
+    char tokens[MAX_LENGTH][MAX_LINES];
+    
+    //OPEN THE FILE
+    FILE *the_file = fopen(file_name,"r");
+
+    //PRINT ERROR IF UNABLE TO OPEN FILE
+    /* Check for data errors in both files - if any errors only the first should be reported and then the program should terminate */
+    if(the_file == NULL){
+        printf("Unable to open the file" '%s'\n", file_name);
+        exit(EXIT_FAILURE);
      }
-     // CHECKING FOR END OF FILE OR ERROR
-     char line [100];
-     while(fgets(line, sizeof(line), the_file)) !=  NULL){
-         char command[10][40];
-         int i = 0;
-         while(line[i] != '\n' ){
-             command[0][i] = line[i];
-     //    char command[10][40];
-     //    int i = 0;
-     //    while(line[i] != '\n' ){
-     //        command[0][i] = line[i];
-     //        i++;
-     //        while(isspace(line[i])){
-     //            printf("%d",line[i]);
-     //            i++;
-     //        }
-     //
-     //    }
-     //    printf("%s\n",command[0]);
+    
+    int i = 0;
+               
+    // CHECK FOR END OF FILE (EOF) OR ANY ERROR WHILE READING
+    char line [MAX_LINES];
+    while(!eof(file) && !ferror(file)){
+            //CHECK IF NUMBER OF LINES < MAX_LINES
+            if(fgets(tokens[i], MAX_LENGTH, the_file) != NULL){
+                i++;
+            }
+        (fgets(line, sizeof(line), the_file)) != NULL){
+        char command[10][40];
+        int i = 0;
+        while(line[i] != '\n' ){
+            command[0][i] = line[i];
+            
+    //CLOSE FILE
+    fclose(file);
+     
+    //    char command[10][40];
+    //    int i = 0;
+    //    while(line[i] != '\n' ){
+    //        command[0][i] = line[i];
+    //        i++;
+    //        while(isspace(line[i])){
+    //            printf("%d",line[i]);
+    //            i++;
+    //        }
+    //
+    //    }
+    //    printf("%s\n",command[0]);
 
          for (char *p = strtok(line," \t\n\0"); p != NULL; p = strtok(NULL, " ")){
                strcpy(tokens[i] , p);
