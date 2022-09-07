468886c5703e186a4f60afbebb06f631e73899d5
22332371 <105031287+22332371@users.noreply.github.com>
2022-09-07 10:56:17 +0800

    Added error for message for opening file error

--- read.c
+++ read.c
@@ -9,9 +9,9 @@
 
 /*
  crontab-file
- minute - int (0 to 59)
- hour - int (0 to 24)
- day - int (0 to 31) this is dependent on the day obviously
+ minutes - int (0 to 59)
+ hours - int (0 to 24)
+ days - int (0 to 31) this is dependent on the day obviously
  month - int (0 to 12)
  day_name - string(Sun, Mon, Tues etc)
  command - string
@@ -41,75 +41,90 @@
  
 */
 
-// struct Program{
-//	int minutes;
-//	char[41] name;
-// };
+
+ struct Program{
+     int minutes;
+     char[41] name;
+ }
+ //struct Program{
+ //    int minutes;
+ //    char[41] name;
+ //};
 
 
-//struct Program programs[20];
+ struct Program programs[20];
+ //struct Program programs[20];
 
-void readfile(char file[]){
-	
-	char tokens[40][100];
-	int i = 0;
-	//OPEN FILE
-	FILE *info = fopen(file,"r");
-	char line [100];
-	while(fgets(line,sizeof line, info) !=  NULL){
-	//	char command[10][40];
-	//	int i = 0;
-	//	while(line[i] != '\n' ){
-	//		command[0][i] = line[i];
-	//		i++;
-	//		while(isspace(line[i])){
-	//			printf("%d",line[i]);
-	//			i++;
-	//		}
-	//		
-	//	}
-	//	printf("%s\n",command[0]);
-		
-		for (char *p = strtok(line," \t\n\0"); p != NULL; p = strtok(NULL, " ")){
-  			strcpy(tokens[i] , p);
-			printf("token=%s\n", p);
-			printf("tokens variable = %s\n",tokens[i]);
-			i++;
-			printf("------------------------\n");
-		}
+ void readfile(char file[]){
 
+     char tokens[40][100];
+     int i = 0;
+     
+     //OPENING THE FILE READ ONLY LINE BY LINE
+     FILE *the_file = fopen(file.txt,"r");
+     //PRINT ERROR IF UNABLE TO OPEN FILE
+     if(the_file == NULL){
+         perror("Unable to open the file");
+     }
+     // CHECKING FOR END OF FILE OR ERROR
+     char line [100];
+     while(fgets(line, sizeof(line), the_file)) !=  NULL){
+         char command[10][40];
+         int i = 0;
+         while(line[i] != '\n' ){
+             command[0][i] = line[i];
+     //    char command[10][40];
+     //    int i = 0;
+     //    while(line[i] != '\n' ){
+     //        command[0][i] = line[i];
+     //        i++;
+     //        while(isspace(line[i])){
+     //            printf("%d",line[i]);
+     //            i++;
+     //        }
+     //
+     //    }
+     //    printf("%s\n",command[0]);
 
+         for (char *p = strtok(line," \t\n\0"); p != NULL; p = strtok(NULL, " ")){
+               strcpy(tokens[i] , p);
+             printf("token=%s\n", p);
+             printf("tokens variable = %s\n",tokens[i]);
+             i++;
+             while(isspace(line[i])){
+                 printf("%d",line[i]);
+                 i++;
+             }
 
-	//	int i = 0;
-	//	char * token = strtok(line, " \t");
-	//	char command[100][40];
-	//	strcpy(command[0],token);
-//		printf("THIS IS THE COMMAND: %s\n",command[0]);
-//		while(token != NULL){
-//			token = strtok(NULL, " \t");
-//			printf("this is the token %s\n",token);
-			//strcpy(command[1],token);
-	}
-
-	
-		//printf("this is the command name: %s\n",command);
-
-	for(int j = 0; j< 8; j++){
-			printf("--test:%s\n",tokens[j]);
-
-		}
-	int num = atoi(tokens[1]);
-	printf("NUMBER : %d\n", num);
-	fclose(info);
-		//printf("LINE content is : %s",line); // print file contents
-	}
-	
-	//CLOSE FILE
+             printf("------------------------\n");
+         }
+         printf("%s\n",command[0]);
 
 
 
-int main (int argc, char *argv[]){
-	readfile(argv[1]);
-	return 0;
-}
+
+     //    int i = 0;
+     //    char * token = strtok(line, " \t");
+     //    char command[100][40];
+ @@ -38,12 +51,17 @@ void readfile(char file[]){
+ //            token = strtok(NULL, " \t");
+ //            printf("this is the token %s\n",token);
+             //strcpy(command[1],token);
+         }
+     }
+
+
+         //printf("this is the command name: %s\n",command);
+
+
+     for(int j = 0; j< 8; j++){
+             printf("--test:%s\n",tokens[j]);
+
+         }
+     int num = atoi(tokens[1]);
+     printf("NUMBER : %d\n", num);
+     fclose(info);
+         //printf("LINE content is : %s",line); // print file contents
+     }
+
 
