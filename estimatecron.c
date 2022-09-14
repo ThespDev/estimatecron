a14181291751b3c5aba1194a802e60ba1189ee1a
ThespDev <pablonaydenovau@gmail.com>
2022-09-14 15:02:11 +0800

    readfile and struct template done

--- estimatecron.c
+++ estimatecron.c
@@ -15,22 +15,82 @@
 #define CURRENT_YEAR 2022
 #define DAYS_PER_WEEK fine 7
 
+//errors:
+//throw error if task in schedule but not estimates
+
+struct Data{
+	char file_info[MAX_COMMAND_COUNT][MAX_LINE_LENGTH];
+};
+// * == -1
+struct ScheduledTask{
+	char programName[MAX_COMMAND_LENGTH + 1];
+	int minute;
+	int hour;
+	int day;
+	int month;
+	int dayofWeek;
+}
+
+struct estimate{
+	char programName[MAX_COMMAND_LENGTH +1];
+	int minutes;
+}
+
+void parseData(struct Data filedata){
+
+}
+
+// OPEN AND READ THE crontab-file AND estimates-file
+struct Data readfile(char file_name[]){
+
+    //STORE THE LINES FROM THE FILE
+	struct Data data;
+    //OPEN THE FILE
+    FILE *file_data = fopen(file_name,"r");
+
+    //PRINT ERROR IF UNABLE TO OPEN FILE
+    /* Check for data errors in both files - if any errors only the first should be reported and then the program should terminate */
+    if(file_data == NULL){
+        printf("Unable to open the file %s\n", file_name);
+        exit(EXIT_FAILURE);
+    }
+
+    int line_number = 0;
+
+    // CHECK FOR END OF FILE (EOF) OR ANY ERROR WHILE READING
+    char line [MAX_LINE_LENGTH];
+    while(fgets(line,sizeof line, file_data) !=  NULL){
+	    strcpy(data.file_info[line_number],line);
+	    line_number++;
+	}
+    //CLOSE FILE
+    fclose(file_data);
+    return data;
+}
+
+//RETURNS THE AMOUNT OF DAYS IN A MONTH BASED OFF INTEGER GIVEN (0-11)
 int daysInMonth(int month) {
+  if (month < 0 || month > 11){
+	fprintf(stderr,"%i is not a valid month\n",month);
+  }
   int daysOfMonths[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
   return daysOfMonths[month];
 }
 
-int first_day_of_month(int month, int year) {
-  struct tm tm;
+int first_day_of_month(int month) {
 
+  struct tm tm;
   //  SET ALL FIELDS OF tm TO ZERO TO MAKE SOME FIELDS 'UNKNOWN'
   memset(&tm, 0, sizeof(tm));
 
   //  INITIALIZE THE FILEDS THAT WE ALREADY KNOW
   tm.tm_mday = 1;
-  tm.tm_mon = month - 1; // 0=Jan, 1=Feb, ....
-  tm.tm_year = year - 1900;
+  tm.tm_mon = month; // 0=Jan, 1=Feb, ....
+	tm.tm_year = CURRENT_YEAR-1900;
   mktime(&tm);
+  printf("CURRENT YEAR %d\n",tm.tm_year);
+  printf("CURRENT MONTH %d\n",tm.tm_mon);
+
   //  RETURN THE INTEGER MONTH VALUE
   return tm.tm_wday; // 0=Sun, 1=Mon, .....
 }
@@ -39,7 +99,7 @@ void simulateMonth(int month) {
   int minute = 0;
   int hour = 0;
   int day = 1;
-  int dayofWeek = first_day_of_month(month, CURRENT_YEAR);
+  int dayofWeek = first_day_of_month(month);
   int minutesinMonth = daysInMonth(month) * 24 * 60;
   for (int i = 0; i < minutesinMonth; i++) {
     printf("Iteration : %d\n Minute: %d , Hour: %d , Day: %d , DayOfWeek: %d\n",
@@ -61,6 +121,7 @@ void simulateMonth(int month) {
 }
 
 int main(int argc, char *argv[]) {
-  simulateMonth(atoi(argv[1]));
+  struct Data filedata = readfile(argv[1]);
+  printf("LINE 1: %s\n",filedata.file_info[0]);
   return 0;
 }
